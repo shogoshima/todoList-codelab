32beddd903446af467aa686e74a5f0cd5a9a8b97
shogoshima <shogoshima@usp.br>
2023-06-19 18:04:47 -0300

    adicionei alguns arquivos pra ajudar a linkar

---
+++ public/index.js
@@ -0,0 +1,3 @@
+// aqui vamos importar módulos js.
+// na hora de criar funções javascript, criar um arquivo novo na mesma pasta,
+// e linka aqui, tipo: import { soma, sub, mult, div } from './modulo.js';
