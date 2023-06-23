04ba237f2044280226b5e9e6dfa8cf6150bf6bde
shogoshima <shogoshima@usp.br>
2023-06-23 00:18:51 -0300

    adicionei cookie para lembrar qm ta logado

--- public/modulos/login.js
+++ public/modulos/login.js
@@ -1,4 +1,8 @@
-
+// funcao para cadastrar:
+// pega os dados do input (user e pswd) que seriam do document.getElementById.
+// cria um objeto chamado dados, em que teria a senha do usuario e um outro objeto,
+// que dessa vez vai ser um objeto com todas as informações das tabelas do todolist
+// apos criar o objeto, ele é transformado em string e é mandado pro localStorage
 export function cadastrar(user, pswd) {
     let dados = {
         senha: pswd,
@@ -15,6 +19,11 @@ export function cadastrar(user, pswd) {
     alert("Usuário cadastrado com sucesso!");
 }
 
+// funcao para logar:
+// com JSON.parse, ele pega os dados salvos da chave "user" e o transforma em um objeto que o JS entende
+// se "dados" não for vazio, ele vê se a senha que tava salva é a mesma q ta no input.
+// se for a mesma, cria um cookie que expira depois de 15 minutos, e vai para o html do todolist
+// senao, ele só diz q ta errado
 export function login(user, pswd) {
     if (user == "" || pswd == "") {
         alert("Parece que você esqueceu de digitar algo kkk");
@@ -25,7 +34,11 @@ export function login(user, pswd) {
         // encontrei dado do usuario, preciso verificar se a senha está correta
         if (dados.senha == pswd) {
             alert(`Bem vindo(a) de volta, ${user}!`);
-            window.location.href = "todolist.html";
+            const d = new Date(); 
+            d.setTime(d.getTime() + (15 * 60 * 1000)) // seta o tempo para 15 minutos depois de agora
+            let expires = "expires=" + d.toUTCString(); // transforma em string
+            document.cookie = "logado=" + user + ";" + expires; // adiciona no formato de um cookie
+            window.location.href = "todolist.html"; 
         } else {
             alert(`${user}, sua senha está errada. Por favor, tente novamente.`);
         }
---
+++ public/todolist.js
@@ -0,0 +1,34 @@
+// para pegar o cookie que foi criado na aba de login.
+// isso é so para conseguir pegar o nome do usuário que logou na aba de login
+let user = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("logado="))
+    ?.split("=")[1];
+
+// Só pra mostrar q assim conseguimos pegar dados do usuario no localStorage
+let dados = JSON.parse(localStorage.getItem(user));
+document.querySelector("#bemvindo").innerHTML = `Oi ${user}! sua senha é "${dados.senha}"`;
+
+// so um exemplo de como acho q seria o objeto com o qual vamos trabalhar
+// a gente teria acesso a ele escrevendo "dados.todolist".
+// conseguimos mudar os dados toda vez q o usuario editar, ou criar um novo objeto na lista de tarefas
+let todolist = {
+    tarefas: [
+        {
+            nome: "cozinhar",
+            data: "20/07/2023",
+            completo: false,
+        },
+        {
+            nome: "fazer tarefa",
+            data: "20/07/2023",
+            completo: false,
+        },
+        {
+            nome: "dormir",
+            data: "20/07/2023",
+            completo: false,
+        }
+    ]
+}
+console.log(todolist.tarefas[1].nome); // printa "fazer tarefa"
