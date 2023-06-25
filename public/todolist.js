0daa6b800f014ef8d1a0c0d6571fc2663143f40a
shogoshima <shogoshima@usp.br>
2023-06-25 19:59:59 -0300

    Mudei algumas coisas no html para colocar função de adição

--- public/todolist.js
+++ public/todolist.js
@@ -9,26 +9,29 @@ let user = document.cookie
 let dados = JSON.parse(localStorage.getItem(user));
 document.querySelector("#bemvindo").innerHTML = `Oi ${user}! sua senha é "${dados.senha}"`;
 
+
+
+
 // so um exemplo de como acho q seria o objeto com o qual vamos trabalhar
 // a gente teria acesso a ele escrevendo "dados.todolist".
 // conseguimos mudar os dados toda vez q o usuario editar, ou criar um novo objeto na lista de tarefas
-let todolist = {
-    tarefas: [
-        {
-            nome: "cozinhar",
-            data: "20/07/2023",
-            completo: false,
-        },
-        {
-            nome: "fazer tarefa",
-            data: "20/07/2023",
-            completo: false,
-        },
-        {
-            nome: "dormir",
-            data: "20/07/2023",
-            completo: false,
-        }
-    ]
-}
-console.log(todolist.tarefas[1].nome); // printa "fazer tarefa"
+// let todolist = {
+//     tarefas: [
+//         {
+//             nome: "cozinhar",
+//             data: "20/07/2023",
+//             completo: false,
+//         },
+//         {
+//             nome: "fazer tarefa",
+//             data: "20/07/2023",
+//             completo: false,
+//         },
+//         {
+//             nome: "dormir",
+//             data: "20/07/2023",
+//             completo: false,
+//         }
+//     ]
+// }
+// console.log(todolist.tarefas[1].nome); // printa "fazer tarefa"
