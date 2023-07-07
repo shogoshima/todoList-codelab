387847b687a8467d94b130a20f0bdbf483c9de00
shogoshima <shogoshima@usp.br>
2023-07-07 00:35:05 -0300

    testando - deploy inicial não funcionou

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static("./public"))
+app.use(express.static("./public"));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
